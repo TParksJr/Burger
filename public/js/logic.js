dfd0bffab121005814a7db2be3ed5ea1c8b4e5bc
Tyson <tys_p@yahoo.ca>
2018-09-27 13:57:40 -0400

    minor changes

--- public/js/logic.js
+++ public/js/logic.js
@@ -21,10 +21,10 @@ $(function () {
       
         var burger_id = $(this).children(".burger_id").val();
         $.ajax({
-          method: "PUT",
-          url: "/burgers/update/" + burger_id
+            method: "PUT",
+            url: "/burgers/update/" + burger_id
         }).then(function(data) {
-          location.reload();
+            location.reload();
         });
     });
 });
